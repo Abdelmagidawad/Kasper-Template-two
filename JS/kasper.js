a171edd3b7008cd342f4174bd134c2a5ad6d8b49
Abdelmagidawad <abdelmagidawad@gmail.com>
2023-08-01 16:35:28 +0300

    add scroll top useing js

--- JS/kasper.js
+++ JS/kasper.js
@@ -1,11 +1,18 @@
+let scrollUp = document.querySelector(".up-icon");
+window.addEventListener("scroll", function () {
+  if (this.scrollY >= 550) {
+    scrollUp.classList.add("show");
+  } else {
+    scrollUp.classList.remove("show");
+  }
+});
+
 let div = document.querySelector(".our-skills");
 let spans = document.querySelectorAll(".prog span");
-
-window.onscroll = function () {
-    if (window.scrollY >= div.offsetTop - 40) {
-        console.log("Reached div our-skills");
-        spans.forEach((span) => {
-            span.style.width = span.dataset.width;
-        });
-    }
-};
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= div.offsetTop - 40) {
+    spans.forEach((span) => {
+      span.style.width = span.dataset.width;
+    });
+  }
+});
